ba4092da2efe31ac8794e523018c72e233feac38
bar.minz <barminz1209@gmail.com>
2024-04-01 15:31:36 +0300

    Fixed another misstype

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "github_sg" {
       from_port   = port.value
       to_port     = port.value
       protocol    = "tcp"
-      cidr_blocks = port.value==["0.0.0.0/0"]
+      cidr_blocks = ["0.0.0.0/0"]
     }
   }
 
